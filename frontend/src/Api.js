efacedd7f1801dce225f8a4a88da534a61e17e71
Naveen Kumar <jangirnaveen2212@gmail.com>
2026-05-20 16:36:15 +0530

    changed connnection string

--- frontend/src/Api.js
+++ frontend/src/Api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://white-board-app-production.up.railway.app/api",
+  baseURL: "http://localhost:3030/api",
   withCredentials: true,
 });
 
